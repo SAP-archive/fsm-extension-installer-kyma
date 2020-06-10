c9d920ac7b9fa4208153c99a1ff14119ea533524
Peter Wang <peter.wang03@sap.com>
2020-06-10 18:11:47 +0800

    Add unit test cases to cover all basic methods.

--- backend/src/chartservice/chartservice.service.spec.ts
+++ backend/src/chartservice/chartservice.service.spec.ts
@@ -1,8 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import fse = require('fs-extra');
 import { ChartserviceService } from './chartservice.service';
+import {CHART_CACHE_PATH} from "../utils/constants";
+import {ChartConfigData} from "../utils/interfaces/chartconfigdata.interface";
+
+jest.mock('download-git-repo');
+jest.mock('uuid');
+import download = require('download-git-repo');
+import {v4 as uuidv4} from 'uuid';
 
 describe('ChartserviceService', () => {
   let service: ChartserviceService;
+
+  // Remove chart_cache folder if this folder is existing.
+  fse.removeSync(CHART_CACHE_PATH);
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -12,7 +23,50 @@ describe('ChartserviceService', () => {
     service = module.get<ChartserviceService>(ChartserviceService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should be called for removeDownloadedPath method', () => {
+    // Existing CHART_CACHE_PATH folder
+    service.removeDownloadedPath(CHART_CACHE_PATH);
+    expect(fse.existsSync(CHART_CACHE_PATH)).toEqual(false);
+
+    // Not exist CHART_CACHE_PATH folder, cover other branch
+    service.removeDownloadedPath(CHART_CACHE_PATH);
+    expect(fse.existsSync(CHART_CACHE_PATH)).toEqual(false);
+  });
+
+  it('should successfully download chart from github repo', async () => {
+    const chartConfigData = {
+      "repository": "https://github.com",
+      "ref": "master",
+      "path": "helm/"
+    } as ChartConfigData;
+    const mockId = 'fd05ed61-faad-4868-8039-e5cd460b7e26';
+
+    download.mockImplementation((repo, dest, opts, fn) => {
+      console.log(repo);
+      console.log(dest);
+      console.log(opts);
+      fn(null);
+    });
+    uuidv4.mockImplementation(() => {return mockId});
+
+    await expect(service.downloadChartFromGithubRepo(chartConfigData)).resolves.toEqual(CHART_CACHE_PATH + mockId);
+  });
+
+  it('should failed download chart from github repo', async () => {
+    const chartConfigData = {
+      "repository": "https://github.com",
+      "ref": undefined,
+      "path": "helm/"
+    } as ChartConfigData;
+    const error = new Error('test reject branch for download method');
+
+    download.mockImplementation((repo, dest, opts, fn) => {
+      console.log(repo);
+      console.log(dest);
+      console.log(opts);
+      fn(error);
+    });
+
+    await expect(service.downloadChartFromGithubRepo(chartConfigData)).rejects.toThrow(error);
   });
 });
--- backend/src/installerservice/installerservice.service.spec.ts
+++ backend/src/installerservice/installerservice.service.spec.ts
@@ -1,18 +1,124 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { InstallerService } from './installerservice.service';
+import {ExtensionCatalogServiceModule} from '../extensioncatalogservice/extensioncatalogservice.module';
+import {ChartServiceModule} from '../chartservice/chartservice.module';
+import {HelmserviceModule} from '../helmservice/helmservice.module';
+import {KubectlModule} from '../kubectl/kubectl.module';
+import {RequestInstallData} from '../utils/interfaces/requestdata.interface';
+import {ExtensionCatalogService} from '../extensioncatalogservice/extensioncatalogservice.service';
+import {DeployConfigData} from '../utils/interfaces/deployconfigdata.interface';
+import {ChartserviceService} from '../chartservice/chartservice.service';
+import {HelmserviceService} from '../helmservice/helmservice.service';
+import {KubectlService} from '../kubectl/kubectl.service';
+
+jest.mock('recursive-search');
+jest.mock('yamljs');
+import search = require('recursive-search');
+import yamljs = require('yamljs');
 
 describe('InstallerService', () => {
   let service: InstallerService;
+  let extensionCatalogService: ExtensionCatalogService;
+  let chartserviceService: ChartserviceService;
+  let helmserviceService: HelmserviceService;
+  let kubectlService: KubectlService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [ExtensionCatalogServiceModule, ChartServiceModule, HelmserviceModule, KubectlModule],
       providers: [InstallerService],
     }).compile();
 
     service = module.get<InstallerService>(InstallerService);
+    extensionCatalogService = module.get<ExtensionCatalogService>(ExtensionCatalogService);
+    chartserviceService = module.get<ChartserviceService>(ChartserviceService);
+    helmserviceService = module.get<HelmserviceService>(HelmserviceService);
+    kubectlService = module.get<KubectlService>(KubectlService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should be successfully invoked for installExtension', async () => {
+    const requestData = {
+      accountId: '1',
+      companyId: '2',
+      extensionDeploymentId: '2af11761-37bb-4aea-bc8a-f2b8ff4cca69'
+    } as RequestInstallData;
+
+    const deployConfigData = {
+      chartConfigData: {
+        path: ''
+      }
+    } as DeployConfigData;
+    const spy4getDeploymentConfigData =
+        jest.spyOn(extensionCatalogService, 'getDeploymentConfigData').mockReturnValueOnce(Promise.resolve(deployConfigData));
+
+    const repoLocalPath = '';
+    const spy4downloadChartFromGithubRepo =
+        jest.spyOn(chartserviceService, 'downloadChartFromGithubRepo').mockReturnValueOnce(Promise.resolve(repoLocalPath));
+
+    const chartPaths = ['/helm/Chart.yaml'];
+    const spy4recursiveSearchSync =
+        jest.spyOn(search, 'recursiveSearchSync').mockReturnValueOnce(chartPaths);
+
+    const chartMetadata = {
+      name: 'test-extension'
+    };
+    const spy4load =
+        jest.spyOn(yamljs, 'load').mockReturnValueOnce(chartMetadata);
+
+    const spy4exist =
+        jest.spyOn(helmserviceService, 'exist').mockReturnValueOnce(Promise.resolve(true));
+    const spy4delete =
+        jest.spyOn(helmserviceService, 'delete').mockReturnThis();
+
+    const installResult = {
+      stderr: null,
+      stdout: {
+        name: 'test-extension',
+        namespace: 'default',
+        info: {
+          notes: 'notes',
+          description: 'description'
+        }
+      }
+    };
+    const spy4install =
+        jest.spyOn(helmserviceService, 'install').mockReturnValueOnce(Promise.resolve(installResult));
+    const spy4jsonParse =
+        jest.spyOn(JSON, 'parse').mockReturnValueOnce(installResult.stdout);
+
+    const accessUrl = `https://console.kongfu.kaka.shoot.canary.k8s-hana.ondemand.com`;
+    const spy4getAccessUrlFromKymaByAppName =
+        jest.spyOn(kubectlService, 'getAccessUrlFromKymaByAppName').mockReturnValueOnce(Promise.resolve(accessUrl));
+
+    const spy4updateDeploymentInfoToCatalog =
+        jest.spyOn(extensionCatalogService, 'updateDeploymentInfoToCatalog').mockReturnThis();
+
+    const spy4addDeploymentResultToCatalog =
+        jest.spyOn(extensionCatalogService, 'addDeploymentResultToCatalog').mockReturnThis();
+
+    const spy4removeDownloadedPath =
+        jest.spyOn(chartserviceService, 'removeDownloadedPath').mockReturnThis();
+
+    await service.installExtension(requestData);
+    expect(spy4getDeploymentConfigData).toBeCalledTimes(1);
+    expect(spy4downloadChartFromGithubRepo).toBeCalledTimes(1);
+    expect(spy4recursiveSearchSync).toBeCalledTimes(1);
+    expect(spy4load).toBeCalledTimes(1);
+    expect(spy4exist).toBeCalledTimes(1);
+    expect(spy4delete).toBeCalledTimes(1);
+    expect(spy4install).toBeCalledTimes(1);
+    expect(spy4jsonParse).toBeCalledTimes(1);
+    expect(spy4getAccessUrlFromKymaByAppName).toBeCalledTimes(1);
+    expect(spy4updateDeploymentInfoToCatalog).toBeCalledTimes(1);
+    expect(spy4addDeploymentResultToCatalog).toBeCalledTimes(1);
+    expect(spy4removeDownloadedPath).toBeCalledTimes(1);
+  });
+
+  it('should be successfully invoked for upgradeExtension', async () => {
+
+  });
+
+  it('should be successfully invoked for uninstallExtension', async () => {
+
   });
 });
